32205413aa4e5a003d2e95f3982bcc2690b448ab
Srishti Jain <srishti@sumologic.com>
2018-04-24 00:39:45 +0530

    Update tests

--- shellbase-slack/src/test/scala/com/sumologic/shellbase/slack/PostCommandToSlackTest.scala
+++ shellbase-slack/src/test/scala/com/sumologic/shellbase/slack/PostCommandToSlackTest.scala
@@ -62,6 +62,22 @@ class PostCommandToSlackTest extends CommonWordSpec with BeforeAndAfterEach with
       verify(chatClient, times(2)).postMessage(matcher_eq(channel), anyString(), anyObject[Map[String, String]]())
     }
 
+    "send the message when multiple channels configured" in {
+      val (_, chatClient) = createMockClient
+      val channels = createTwoChannels
+      sut.slackMessagingConfigured should be (true)
+
+      sut.postCommandToSlack(List("Hi"), List.empty) should be (None)
+      channels.foreach(channel =>
+        verify(chatClient, times(1)).postMessage(matcher_eq(channel), anyString(), anyObject[Map[String, String]]())
+      )
+
+      sut.postCommandToSlack(List("Hi"), List("with", "params")) should be (None)
+      channels.foreach(channel =>
+        verify(chatClient, times(2)).postMessage(matcher_eq(channel), anyString(), anyObject[Map[String, String]]())
+      )
+    }
+
     "return an exception as text when thrown" in {
       val (slackClient, _) = createMockClient
       val channel = createAChannel
@@ -95,7 +111,14 @@ class PostCommandToSlackTest extends CommonWordSpec with BeforeAndAfterEach with
   private def createAChannel: String = {
     val channel = "#my_channel"
     when(mockState.slackChannel).thenReturn(Some(channel))
+    when(mockState.slackChannels).thenReturn(Some(channel).toList)
     channel
+  }
+
+  private def createTwoChannels: List[String] = {
+    val channels = List("#my_channel1", "#my_channel2")
+    when(mockState.slackChannels).thenReturn(channels)
+    channels
   }
 
   var sut: PostCommandToSlack = _
@@ -106,6 +129,7 @@ class PostCommandToSlackTest extends CommonWordSpec with BeforeAndAfterEach with
 
     when(mockState.slackClient).thenReturn(None)
     when(mockState.slackChannel).thenReturn(None)
+    when(mockState.slackChannels).thenReturn(None.toList)
     when(mockState.slackOptions).thenReturn(new SlackState {
       override def slackClient: Option[SlackClient] = ???
       override def slackChannel: Option[String] = ???
